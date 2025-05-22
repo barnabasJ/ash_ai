5d6e19bc83cb12ff91ef6da1f8bf98a5897d1683
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2025-05-22 22:24:09 +0200

    feat: make the dev mcp path configurable

--- lib/ash_ai/mcp/dev.ex
+++ lib/ash_ai/mcp/dev.ex
@@ -1,18 +1,39 @@
 defmodule AshAi.Mcp.Dev do
-  @moduledoc "Place in your endpoint's code_reloading section to expose Ash dev MCP at /mcp/ash"
+  @moduledoc """
+  Place in your endpoint's code_reloading section to expose Ash dev MCP"
+
+  Default path is `/mcp/ash`
+  """
   @behaviour Plug
 
   @impl true
   def init(opts) do
-    AshAi.Mcp.Router.init(Keyword.put(opts, :tools, :ash_dev_tools))
+    path =
+      opts
+      |> Keyword.get(:path, "/mcp/ash")
+      |> String.split("/")
+      |> Enum.reject(&(&1 == ""))
+
+    opts =
+      opts
+      |> Keyword.put(:tools, :ash_dev_tools)
+      |> Keyword.put(:path, path)
+
+    AshAi.Mcp.Router.init(opts)
   end
 
   @impl true
-  def call(%Plug.Conn{path_info: ["mcp", "ash" | rest]} = conn, opts) do
-    conn
-    |> Plug.forward(rest, AshAi.Mcp.Router, opts)
-    |> Plug.Conn.halt()
-  end
+  def call(%Plug.Conn{path_info: path_info} = conn, opts) do
+    expected_path = Keyword.get(opts, :path)
 
-  def call(conn, _opts), do: conn
+    if List.starts_with?(path_info, expected_path) do
+      rest = Enum.drop(path_info, length(expected_path))
+
+      conn
+      |> Plug.forward(rest, AshAi.Mcp.Router, opts)
+      |> Plug.Conn.halt()
+    else
+      conn
+    end
+  end
 end
